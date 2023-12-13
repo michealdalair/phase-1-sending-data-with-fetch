ff0ccb23902dec7915814cc456461941f5be36b1
michaljones <micheal.jones2010@yahoo.com>
2023-12-12 21:12:17 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Add your code here
+function submitData(name, email) {
+    const configurationObject = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+      },
+      body: JSON.stringify({
+        name: name,
+        email: email,
+      }),
+    };
+  
+    return fetch("http://localhost:3000/users", configurationObject)
+      .then(function (response) {
+        if (!response.ok) {
+          throw new Error(`Network response was not ok: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then(function (data) {
+        const newId = data.id;
+        //const newIdElement = document.createElement("p");
+        //newIdElement.textContent = `New ID: ${newId}`;
+        document.body.textContent= newId
+    
+      })
+      .catch(function (error) {
+      //const errorMessageElement = document.createElement("p");
+        //errorMessageElement.textContent = `Error: ${error.message}`;
+        document.body.innerHTML= error.message
+        //throw error;
+      });
+  }
+  
